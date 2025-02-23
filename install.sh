c1c61186043a63b3a9f7705532078b36f0630905
Nick Klein <1294247+nickklein@users.noreply.github.com>
2025-02-22 19:16:07 -0800

    Yearly nvim upgrade

--- install.sh
+++ install.sh
@@ -5,47 +5,47 @@ DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 read -p "Replace existing Neovim config inside /.config/nvim?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    # Neovim
-    rm -rf $HOME/.config/nvim
-    ln -s $DOTFILES/nvim $HOME/.config/nvim
+  # Neovim
+  rm -rf $HOME/.config/nvim
+  ln -s $DOTFILES/nvim2 $HOME/.config/nvim
 fi
 
 read -p "Replace existing gitconfig config inside /HOME/.gitconfig?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    # Git
-    rm -rf $HOME/.gitconfig
-    ln -sf $DOTFILES/git/gitconfig $HOME/.gitconfig
+  # Git
+  rm -rf $HOME/.gitconfig
+  ln -sf $DOTFILES/git/gitconfig $HOME/.gitconfig
 fi
 
 read -p "Replace existing tmux config inside /HOME/.tmux.conf?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    #tmux
-    rm -rf $HOME/.tmux.conf
-    ln -sf $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+  #tmux
+  rm -rf $HOME/.tmux.conf
+  ln -sf $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
 fi
 
 #bash
 read -p "Replace existing bash inside /HOME/.bashrc?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    rm -rf $HOME/.bashrc
-    ln -sf $DOTFILES/bash/bashrc $HOME/.bashrc
+  rm -rf $HOME/.bashrc
+  ln -sf $DOTFILES/bash/bashrc $HOME/.bashrc
 fi
 
 #bash aliases
 read -p "Replace existing bash_aliases inside /HOME/.bash_aliases?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    rm -rf $HOME/.bash_aliases
-    ln -sf $DOTFILES/bash/bash_aliases $HOME/.bash_aliases
+  rm -rf $HOME/.bash_aliases
+  ln -sf $DOTFILES/bash/bash_aliases $HOME/.bash_aliases
 fi
 
 # mycli
 read -p "Replace existing mycli inside /HOME/.my.cnf?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    rm -rf $HOME/.my.cnf
-    ln -sf $DOTFILES/mycli/myclicnf $HOME/.my.cnf
+  rm -rf $HOME/.my.cnf
+  ln -sf $DOTFILES/mycli/myclicnf $HOME/.my.cnf
 fi
---
+++ nvim2/lua/config/keymaps.lua
@@ -0,0 +1,31 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", "<leader>Q", ":bufdo bdelete<CR>")
+vim.keymap.set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+vim.keymap.set("n", "gtd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "gd", ":Telescope lsp_definitions<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "gd", function()
+    require("telescope.builtin").lsp_definitions()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "gi", ":Telescope lsp_implementations<CR>")
+vim.keymap.set("n", "gr", ":Telescope lsp_references<CR>")
+
+vim.keymap.set("n", "<Up>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<Down>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<Left>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<Right>", "<Nop>", { silent = true })
+
+vim.keymap.set("i", "<Up>", "<Nop>", { silent = true })
+vim.keymap.set("i", "<Down>", "<Nop>", { silent = true })
+vim.keymap.set("i", "<Left>", "<Nop>", { silent = true })
+vim.keymap.set("i", "<Right>", "<Nop>", { silent = true })
+
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+-- Close all open buffers.
+vim.keymap.set("n", "<leader>Q", ":bufdo bdelete<CR>")
+
+-- Paste replace visual selection without copying it.
+vim.keymap.set("v", "p", '"_dP')
